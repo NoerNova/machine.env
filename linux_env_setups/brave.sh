76511b93edb0d194f13dbf56f033e5b2b53a5214
NoerNova <noernova666@gmail.com>
2021-07-27 04:16:16 +0700

    Brave Browser

---
+++ linux_env_setups/brave.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e
+
+# Brave Browser
+# Author: noernova
+# Date: Jul 25, 2021
+# Contact: noernova.com
+# Github: https://github.com/noernova
+# ===========================================================================
+
+echo ""
+echo "######################"
+echo "### Brave Browser ###"
+echo "######################"
+echo ""
+
+# SETUP Temp folder
+SETUPDIR=$(pwd)
+[ ! -d $SETUPDIR/temp ] && mkdir $SETUPDIR/temp
+SETUPTEMP=$SETUPDIR/temp
+
+# Update
+echo ""
+echo "### Updating ... ####"
+echo ""
+
+sudo apt update
+
+# Install require modules and packages
+sudo apt install curl apt-transport-https --yes
+
+
+# ----------------------------------------------------------------------------
+
+echo "Brave Browser ..."
+
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+
+sudo apt install brave-browser --yes
+
+cd $SETUPDIR
+
+# Clean temps
+rm -rf $SETUPTEMP
+SETUPTEMP=
+SETUPDIR=
+
+echo "Brave Browser, installation completed."
+# ----------------------------------------------------------------------------
