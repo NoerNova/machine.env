5acffdb57588d4bfd81de9b0887b22a48ae8a9ff
NoerNova <noernova666@gmail.com>
2025-01-29 00:59:14 +0700

    Update: fish latest config

--- fish/config.fish
+++ fish/config.fish
@@ -16,12 +16,13 @@ alias ll "ls -l"
 alias lla "ll -A"
 alias cls "clear"
 alias g git
+alias lv "lvim"
 command -qv nvim && alias vim nvim
 
 # vim key binding
 fish_vi_key_bindings
 
-set -gx EDITOR nvim
+set -gx EDITOR lvim
 
 set -gx PATH bin $PATH
 set -gx PATH ~/bin $PATH
@@ -62,9 +63,60 @@ if test -f $LOCAL_CONFIG
 end
 
 fish_add_path $HOME/.tmux/scripts/
-
+fish_add_path /opt/homebrew/bin
+fish_add_path $HOME/.cargo/env
 source ~/.bash_aliases
+
+export DBUS_SESSION_BUS_ADDRESS="unix:path=$DBUS_LAUNCHD_SESSION_BUS_SOCKET"
+
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-status is-interactive && eval /usr/local/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+end
 # <<< conda initialize <<<
+
+
+# pnpm
+set -gx PNPM_HOME "/Users/noernova/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# ruby
+fish_add_path /opt/homebrew/opt/ruby/bin
+# fish_add_path /opt/homebrew/lib/ruby/gems/3.4.0/bin
+
+set -gx LDFLAGS "-L/opt/homebrew/opt/ruby/lib"
+set -gx CPPFLAGS "-I/opt/homebrew/opt/ruby/include"
+
+set -gx PKG_CONFIG_PATH "/opt/homebrew/opt/ruby/lib/pkgconfig"
+# ruby end
+
+# flutter
+fish_add_path $HOME/.flutter/bin
+# flutter end
+
+# JAVA_HOME
+set -gx JAVA_HOME /Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+
+# ANDROID
+set -gx ANDROID_SDK_ROOT $HOME/Library/Android/sdk
+set -gx ANDROID_HOME $ANDROID_SDK_ROOT
+set -gx PATH $ANDROID_HOME/emulator $PATH
+set -gx PATH $ANDROID_HOME/tools $PATH
+set -gx PATH $ANDROID_HOME/tools/bin $PATH
+set -gx PATH $ANDROID_HOME/platform-tools $PATH
+
+fzf --fish | source
+
+function y
+	set tmp (mktemp -t "yazi-cwd.XXXXXX")
+	yazi $argv --cwd-file="$tmp"
+	if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+		builtin cd -- "$cwd"
+	end
+	rm -f -- "$tmp"
+end
